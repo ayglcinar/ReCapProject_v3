f7beb847d879615bcd804749f2feeb4ecc4fddd3
ayglcinar <60033267+ayglcinar@users.noreply.github.com>
2021-02-08 23:00:19 +0300

    Add files via upload

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetCarTest();
+            GetCarsByBrand();
+
+        }
+
+        private static void GetCarsByBrand()
+        {
+            CarManager carManager = new CarManager(new EfCarDal());
+
+            foreach (var x in carManager.GetCarsByBrandId()
+            {
+                Console.WriteLine(x.CarName);
+            }
+        }
+
+        private static void GetCarTest()
+        {
+            CarManager carManager = new CarManager(new EfCarDal());
+
+            foreach (var x in carManager.GetCarDetails()
+            {
+                Console.WriteLine(x.CarName + "/" + x.BrandName);
+            }
+        }
+    }
+}
