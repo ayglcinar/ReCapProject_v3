1dea6c8ffb89ef9f1f2f4f6238a6f9c6d9a77d13
ayglcinar <60033267+ayglcinar@users.noreply.github.com>
2021-02-08 22:59:38 +0300

    Add files via upload

---
+++ Business/Abstract/ICarService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Entities.Concrete;
+using Entities.DTOs;
+
+namespace Business.Abstract
+{
+    public interface ICarService
+    {
+        List<Car> GetAll();
+        List<Car> GetCarsByColorId(int id);
+        List<Car> GetCarsByBrandId(int id);
+        List<CarDetailDto> GetCarDetails();
+    }
+}
---
+++ Business/Concrete/CarManager.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.DTOs;
+
+namespace Business.Concrete
+{
+    public class CarManager : ICarService
+    {
+        ICarDal _carDal;
+        public CarManager(ICarDal carDal)
+        {
+            this._carDal = carDal;
+        }
+
+        public List<Car> GetAll()
+        {
+            return _carDal.GetAll();
+        }
+
+        public void Add(Car car)
+        {
+            if (car.DailyPrice > 0 && car.CarName.Length>2)
+            {
+                _carDal.Add(car);
+                Console.WriteLine("Araba basariyla eklendi.");
+            }
+            else
+            {
+                Console.WriteLine("Lutfen gunluk fiyat kismini 0'dan buyuk giriniz ve/veya araba ismi 2 karakterden olusmalidir.");
+            }
+        }
+
+        public List<Car> GetCarsByBrandId(int id)
+        {
+            return _carDal.GetAll(p => p.BrandID == id);
+        }
+
+        public List<Car> GetCarsByColorId(int id)
+        {
+            return _carDal.GetAll(p => p.ColorID == id);
+        }
+
+        List<CarDetailDto> ICarService.GetCarDetails()
+        {
+            return _carDal.GetCarDetails();
+        }
+    }
+}
